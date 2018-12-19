21d59a57f3221705bb45fc0abdc74db68e3c0f6c
marupeke_ikd <ikeyan@s2.dion.ne.jp>
2018-12-19 21:30:15 +0900

    高さライン表示を追加

---
+++ develop/unity/prj/FuwaFuwaFur/Assets/code/HeightLine.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightLine : MonoBehaviour {
+
+    [SerializeField]
+    Transform target_;
+
+    [SerializeField]
+    float offset_;
+
+    [SerializeField]
+    TextMesh fontText_;
+
+    [SerializeField]
+    float fontOffsetX_;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetPos = target_.position;
+        float targetY = targetPos.y + offset_;
+        Vector3 pos = transform.position;
+        if ( pos.y < targetY )
+            pos.y = targetY;
+        transform.position = pos;
+
+        Vector3 fontPos = fontText_.transform.position;
+        fontPos.x = targetPos.x + fontOffsetX_;
+        fontText_.transform.position = fontPos;
+
+        fontText_.text = string.Format( "{0:0.###}m", pos.y / 1000.0f );
+    }
+}
