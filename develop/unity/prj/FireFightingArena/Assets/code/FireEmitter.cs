f5407a39e3b733900bccbe801c550175cd7b598f
marupeke_ikd <ikeyan@s2.dion.ne.jp>
2019-01-13 19:12:00 +0900

    FireFightingArena作成環境アップ

---
+++ develop/unity/prj/FireFightingArena/Assets/code/FireEmitter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireEmitter : MonoBehaviour {
+
+    [SerializeField]
+    FireBar fireBar_;
+
+    // 炎を指定方向へ伸長する
+    public void fire( FireBar.FireDirection dir, Vector3 pos, float speed, float dist )
+    {
+        FireBar obj = Instantiate<FireBar>( fireBar_ );
+        obj.fire( dir, pos, speed, dist );
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
