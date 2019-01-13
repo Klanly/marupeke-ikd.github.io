104ed8b8b3aa30d1b14a3f8670d931e4c23ca0b9
marupeke_ikd <ikeyan@s2.dion.ne.jp>
2019-01-13 21:04:40 +0900

    スポットライト用スプライト追加。ビルボーディング処理組み込み。

---
+++ develop/unity/prj/AbnormalBridge/Assets/codes/common/ImageBillboarding.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// イメージをビルボーディングする
+//  イメージじゃなくても使えますよ
+
+public class ImageBillboarding : MonoBehaviour {
+
+    [SerializeField]
+    GameObject target_;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        target_.transform.rotation = Quaternion.LookRotation( Camera.main.transform.position );
+	}
+}
