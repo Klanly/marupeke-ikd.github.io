1a849b7f287fff7415a9cc50b2fb28eb5122c406
marupeke_ikd <ikeyan@s2.dion.ne.jp>
2019-01-26 17:44:47 +0900

    AsterismNightの制作環境をアップ

---
+++ develop/unity/prj/AsterismNight/Assets/codes/common/Table.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// テーブルアクセスクラス
+//
+//  sheetconverterが出力したtsvデータを読み込んでテーブル化
+public class Table {
+
+    // 一時値
+    protected class Val
+    {
+        public int iVal_;
+        public float fVal_;
+        public string sVal_;
+    }
+
+    // テーブルを同期読みで作成
+    //  tableName: tsvファイル名
+    protected bool create( string tableName )
+    {
+        var text = ResourceLoader.getInstance().loadSync<TextAsset>( tableName );
+        if ( text == null ) {
+            return false;
+        }
+
+        string[] lines = text.text.Split( '\n' );
+        if ( lines.Length < 4 ) {
+            // パラメータ数、データ数、パラメータ、型が揃っていない
+            return false;
+        }
+
+        var paramNum = ToVal.Conv.toInt( lines[ 0 ], 0 );
+        var dataNum = ToVal.Conv.toInt( lines[ 1 ], 0 );
+        string[] parameters = lines[ 2 ].Split( '\t' );
+        string[] types = lines[ 3 ].Split( '\t' );
+
+        if (
+            paramNum == 0 ||
+            parameters.Length != paramNum ||
+            types.Length != paramNum ||
+            lines.Length != dataNum + 4
+        ) {
+            // パラメータが設定されていない
+            // データ数が合っていない
+            return false;
+        }
+
+        // データタイプに沿ったインデックスをセット
+        Dictionary<string, Val> dict_ = new Dictionary<string, Val>();
+        int[] typeIdx = new int[ types.Length ];
+        for ( int i = 0; i < types.Length; ++i ) {
+            switch ( types[ i ] ) {
+                case "int": typeIdx[ i ] = 0; break;
+                case "float": typeIdx[ i ] = 1; break;
+                case "string": typeIdx[ i ] = 2; break;
+                default: typeIdx[ i ] = 0; break;
+            }
+            dict_.Add( parameters[ i ], new Val() );
+        }
+
+        // データ格納前コール
+        preStore( dataNum );
+
+        for ( int i = 4; i < lines.Length; ++i ) {
+            string[] datas = lines[ i ].Split( '\t' );
+            if ( datas.Length != paramNum )
+                return false;
+            for ( int j = 0; j < datas.Length; ++j ) {
+                switch ( typeIdx[ j ] ) {
+                    case 0: dict_[ parameters[ j ] ].iVal_ = ToVal.Conv.toInt( datas[ j ], 0 ); break;
+                    case 1: dict_[ parameters[ j ] ].fVal_ = ToVal.Conv.toFloat( datas[ j ], 0 ); break;
+                    case 2: dict_[ parameters[ j ] ].sVal_ = datas[ j ]; break;
+                }
+            }
+
+            // 値を格納者に渡す
+            storeData( dict_ );
+        }
+
+        return true;
+    }
+
+    // データ格納前コール
+    //  派生クラスで必要に応じて使用
+    protected virtual void preStore( int dataNum )
+    {
+    }
+
+    // 1レコードを格納
+    //  派生クラスで具体的な格納を行う
+    protected virtual void storeData( Dictionary<string, Val> values )
+    {
+    }
+}
---
+++ develop/unity/prj/AsterismNight/Assets/codes/tables/Table_asterism_ast.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 星座基礎データ
+
+public class Table_asterism_ast : Table {
+
+    private Table_asterism_ast() { }
+
+    static public Table_asterism_ast getInstance()
+    {
+        if ( table_ == null ) {
+            table_ = new Table_asterism_ast();
+            table_.create( "tables/asterism_ast.tsv" );
+        }
+        return table_;
+    }
+
+    public class Data
+    {
+        public string shortName_;  // 略名
+        public string name_;       // 名前（英語）
+        public string jpName_;     // 名前（日本名）
+        public int longHour_;      // 経度時
+        public int longMinute_;    // 経度分
+        public int lat_;           // 緯度
+    }
+
+    // データ格納前コール
+    protected override void preStore(int dataNum)
+    {
+        data_ = new Data[ dataNum ];
+    }
+
+    // 1レコードを格納
+    //  派生クラスで具体的な格納を行う
+    protected override void storeData( Dictionary<string, Val> values )
+    {
+        var d = new Data();
+        d.shortName_ = values[ "shortName" ].sVal_;
+        d.name_ = values[ "name" ].sVal_;
+        d.jpName_ = values[ "jpName" ].sVal_;
+        d.longHour_ = values[ "longHour" ].iVal_;
+        d.longMinute_ = values[ "longMinute" ].iVal_;
+        d.lat_ = values[ "lat" ].iVal_;
+
+        data_[ dataNum_ ] = d;
+        dataNum_++;
+    }
+
+    // データを取得
+    public Data getData( int index )
+    {
+        if ( index >= data_.Length )
+            return null;
+        return data_[ index ];
+    }
+
+    static Table_asterism_ast table_;
+    int dataNum_ = 0;
+    Data[] data_;
+}
