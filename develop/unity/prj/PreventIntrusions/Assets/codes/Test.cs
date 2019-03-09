ff8de14569846ace9893167e3c7c3431e4601b2b
marupeke_ikd <ikeyan@s2.dion.ne.jp>
2019-03-10 01:00:01 +0900

    PreventIntrusions製作環境アップ

---
+++ develop/unity/prj/PreventIntrusions/Assets/codes/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		List<GlobalState> states = new List<GlobalState>();
+		for ( int i = 0; i < 100; ++i ) {
+			float sec = Random.value * 6.0f;
+			states.Add( GlobalState.wait( sec, () => { Debug.Log( "Finish " + sec ); return false; } ) );
+		}
+		GlobalState.parallel( states.ToArray() ).finish( () => {
+			Debug.Log( "Finish All" );
+		} );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
