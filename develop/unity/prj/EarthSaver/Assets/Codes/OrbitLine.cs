850c538c73fe449e39617a81746f866e4516905f
marupeke_ikd <ikeyan@s2.dion.ne.jp>
2019-05-03 15:32:48 +0900

    隕石の軌道計算とライン描画

---
+++ develop/unity/prj/EarthSaver/Assets/Codes/OrbitLine.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitLine : MonoBehaviour {
+
+    [SerializeField]
+    TrailLine trailLine_;
+
+    [SerializeField]
+    Vector3 initPos_;
+
+    [SerializeField]
+    Vector3 initVec_;
+
+    [SerializeField]
+    float gravity_ = 9.81f;
+
+    [SerializeField]
+    float planetaryRadius_ = 1.0f; // 惑星半径
+
+    [SerializeField]
+    float targetHeight_ = 2.0f;     // ターゲット高
+
+    [SerializeField]
+    float width_ = 0.025f;
+
+    [SerializeField]
+    float stepSec_ = 0.02f;
+
+	// Use this for initialization
+	void Start () {
+        // 軌道計算
+        var data = FallLine.calcOrbit( planetaryRadius_, initPos_, initVec_, gravity_, stepSec_, targetHeight_ );
+
+        // ライン作成
+        trailLine_.setup( data.orbit_, width_ );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ develop/unity/prj/EarthSaver/Assets/Codes/TrailLine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 外部指定トレイルライン
+public class TrailLine : MonoBehaviour {
+
+    [SerializeField]
+    LineRenderer renderer_;
+
+    public void setup( List<Vector3> positions, float width ) {
+        renderer_.positionCount = positions.Count;
+        renderer_.SetPositions( positions.ToArray() );
+        renderer_.startWidth = width;
+        renderer_.endWidth = width;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
