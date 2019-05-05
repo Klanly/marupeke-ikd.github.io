ef66c305df40234ad9dc59d28432861cda277b83
marupeke_ikd <ikeyan@s2.dion.ne.jp>
2019-05-05 11:56:05 +0900

    軌道ラインに当たり判定を付けた

--- develop/unity/prj/EarthSaver/Assets/Codes/SiteManager.cs
+++ develop/unity/prj/EarthSaver/Assets/Codes/SiteManager.cs
@@ -9,6 +9,9 @@ public class SiteManager : MonoBehaviour {
     [SerializeField]
     Transform orbitRoot_;
 
+    [SerializeField]
+    GameObject cursorObj_;
+
     public class Parameter {
         public float orbitPointHeightRange_ = 1.5f;
         public OrbitLine orbitLine_;
@@ -40,13 +43,16 @@ public class SiteManager : MonoBehaviour {
         var data = param.orbitLine_.getData();
         var list = new List<Vector3>();
         for ( int i = data.orbit_.Count - 1; i >= 0; --i ) {
-            if ( data.orbit_[ i ].magnitude <= param.orbitPointHeightRange_ )
+            if ( data.orbit_[ i ].magnitude <= param.orbitPointHeightRange_ ) {
                 list.Add( data.orbit_[ i ] );
+            }
         }
         if ( list.Count <= 2 ) {
             return false;   // 対象となる軌道が無い
         }
-
+        for ( int i = 1; i < data.orbit_.Count; ++i ) {
+            segments_.Add( new Segment( data.orbit_[ i - 1 ], data.orbit_[ i ] ) );
+        }
         var normal = Vector3.Cross( list[ 1 ] - list[ 0 ], list[ 2 ] - list[ 0 ] ).normalized;
         aabb_.set( list );
         CameraUtil.fitAABB( Camera.main, normal, aabb_.Center.normalized, aabb_, out cameraPos_, out cameraQ_ );
@@ -54,7 +60,7 @@ public class SiteManager : MonoBehaviour {
         Camera.main.transform.position = cameraPos_;
         Camera.main.transform.rotation = cameraQ_;
 
-        // DebugArrowLine.create( aabb_.Center, aabb_.Center + normal * 10.0f );
+        debugLine_ =DebugArrowLine.create( aabb_.Center, aabb_.Center + normal * 10.0f );
         return true;
     }
 
@@ -71,11 +77,37 @@ public class SiteManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        // ラインとのコリジョン
+        Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+        cursorSeg_.Start = ray.origin;
+        cursorSeg_.End = cursorSeg_.Start + ray.direction * 10.0f;
+
+        debugLine_.Start = cursorSeg_.Start;
+        debugLine_.End = cursorSeg_.End;
+
+        Vector3 cp = Vector3.zero;
+        Vector3 rayCp = Vector3.zero;
+        float width = 0.03f;
+        float minDist = float.MaxValue;
+        foreach( var seg in segments_ ) {
+            float s = 0.0f;
+            float t = 0.0f;
+            Vector3 tmpCp = Vector3.zero;
+            float dist = seg.calcDistSegment( cursorSeg_, out s, out t, out tmpCp, out rayCp );
+            if ( dist <= width && dist < minDist ) {
+                minDist = dist;
+                cp = tmpCp;
+                break;
+            }
+        }
+        cursorObj_.transform.position = cp;
+    }
 
     Parameter param_;
     AABB aabb_ = new AABB();
     Vector3 cameraPos_ = Vector3.zero;
     Quaternion cameraQ_ = Quaternion.identity;
+    List<Segment> segments_ = new List<Segment>();
+    Segment cursorSeg_ = new Segment();
+    DebugArrowLine debugLine_;
 }
