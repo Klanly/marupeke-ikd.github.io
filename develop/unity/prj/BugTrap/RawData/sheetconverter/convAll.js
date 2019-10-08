517f0b4be57b823ae0848b0a41b5864e4aa6160d
marupeke_ikd <ikeyan@s2.dion.ne.jp>
2019-10-08 09:33:21 +0900

    SheetConverter追加

---
+++ develop/unity/prj/BugTrap/RawData/sheetconverter/convAll.js
@@ -0,0 +1,29 @@
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var wsh = new ActiveXObject("WScript.Shell");
+var curPath = fso.getParentFolderName(WScript.ScriptFullName);
+
+// コンバート
+var files = fso.GetFolder( curPath ).Files;
+var e = new Enumerator( files );
+for ( ; !e.atEnd(); e.moveNext() ) {
+	var file = e.item();
+	var ext = fso.GetExtensionName( file.Name );
+	if ( ext == "xlsx" ) {
+		if ( file.Name.charAt(0) == '~' ) {
+			continue;
+		}
+		WScript.Echo( file.Name );
+		var oExe = wsh.Exec( "sheetconverter.exe -i " + curPath + "\\" + file.Name + " -p unity" );
+		while( oExe.Status == 0 ) {
+		}
+		WScript.Echo( oExe.StdOut.ReadAll() );
+	}
+}
+
+// ファイルを移動(.bytes, .cs)
+fso.CopyFile( curPath + "\\*.bytes", curPath + "\\..\\..\\Assets\\Resources\\Table" );
+fso.CopyFile( curPath + "\\*.cs", curPath + "\\..\\..\\Assets\\Codes\\Table" );
+
+// ファイル削除
+fso.DeleteFile( curPath + "\\*.bytes" )
+fso.DeleteFile( curPath + "\\*.cs" )
