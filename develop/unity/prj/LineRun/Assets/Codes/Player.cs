c745e7bebcf7d491937bcd64d28303e992366a7f
marupeke_ikd <ikeyan@s2.dion.ne.jp>
2019-10-19 19:07:07 +0900

    柵を立てながら移動するようになった

--- develop/unity/prj/LineRun/Assets/Codes/Player.cs
+++ develop/unity/prj/LineRun/Assets/Codes/Player.cs
@@ -23,18 +23,30 @@ public class Player : MonoBehaviour
 	[SerializeField]
 	Field field_;
 
-    // Start is called before the first frame update
-    void Start()
+	[SerializeField]
+	float scrollSpeed_ = 1.0f;
+
+	[SerializeField]
+	Railling raillingPrefab_ = null;
+
+	[SerializeField]
+	float raillingLen_ = 2.0f;
+
+
+	// Start is called before the first frame update
+	void Start()
     {
-        
+		preNodePos_ = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
 		// 上下左右移動
+		// +スクロール
 		Vector3 dir = input_.getDirection();
 		var p = transform.localPosition + dir * speed_ * Time.deltaTime;
+		p.y += scrollSpeed_ * Time.deltaTime;
 		if ( p.x < field_.Left ) {
 			p.x += field_.Width;
 		} else if ( p.x > field_.Right ) {
@@ -45,6 +57,7 @@ public class Player : MonoBehaviour
 		} else if ( p.y > field_.Top ) {
 			p.y -= field_.Height;
 		}
+
 		transform.localPosition = p;
 
 		// ジャンプ
@@ -56,6 +69,9 @@ public class Player : MonoBehaviour
 		} else {
 			jumpState_();
 		}
+
+		// 柵作成チェック
+		checkRaillingCreation();
 
 //		var p = transform.localPosition;
 //		var tex = gameManager_.getLineRecordTexture();
@@ -79,7 +95,59 @@ public class Player : MonoBehaviour
 		transform.localPosition = p;
 	}
 
+	void checkRaillingCreation()
+	{
+		var p = transform.position;
+		var p2 = new Vector2( p.x, p.y );
+		float l = ( p2 - preNodePos_ ).magnitude;
+		if ( l >= raillingLen_ ) {
+			if ( l <= raillingLen_ + 1.0f ) {
+				// フィールド内に収まっている柵
+				var railling = PrefabUtil.createInstance( raillingPrefab_, field_.transform );
+				railling.create( preRail_, p2, -p.z + 1.0f );
+				preNodePos_ = p;
+				preRail_ = railling;
+			} else {
+				// ワープ先なので補正
+				Vector2[] ofs = new Vector2[ 8 ] {
+					new Vector2( -field_.Width, -field_.Height ),
+					new Vector2(  field_.Width, -field_.Height ),
+					new Vector2( -field_.Width,  field_.Height ),
+					new Vector2(  field_.Width,  field_.Height ),
+					new Vector2( -field_.Width, 0.0f ),
+					new Vector2(  field_.Width, 0.0f ),
+					new Vector2( 0.0f, -field_.Height ),
+					new Vector2( 0.0f,  field_.Height )
+				};
+				int idx = 0;
+				float f = 1000000.0f;
+				Vector3 adjPos = Vector3.zero;
+				for ( int i = 0; i < 8; ++i ) {
+					float d = ( p2 + ofs[ i ] - preNodePos_ ).magnitude;
+					if ( d < f ) {
+						adjPos = p2 + ofs[ i ];
+						f = d;
+						idx = i;
+					}
+				}
+				if (f >= raillingLen_ && f <= raillingLen_ + 1.0f ) {
+					var railling = PrefabUtil.createInstance( raillingPrefab_, field_.transform );
+					railling.create( preRail_, adjPos, -p.z + 1.0f );
+					preNodePos_ = p;
+					var railling2 = PrefabUtil.createInstance( raillingPrefab_, field_.transform );
+					railling2.create( null, p, -p.z + 1.0f );
+					preRail_ = railling2;
+				}
+
+				// TODO:
+				// 境界を跨ぐ柵をワープ先側にも作成
+			}
+		} 
+	}
+
 	System.Action jumpState_ = null;
 	bool bJumping_ = false;
 	float jumpTime_ = 0.0f;
+	Vector2 preNodePos_ = Vector2.zero;
+	Railling preRail_ = null;
 }
---
+++ develop/unity/prj/LineRun/Assets/Codes/Railling.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 柵
+
+public class Railling : MonoBehaviour
+{
+	[SerializeField]
+	GameObject poleWood_;
+
+	[SerializeField]
+	GameObject railWood_;
+
+	public float PoleHeight { get { return poleHeight_; } }
+	float poleHeight_ = 1.0f;
+
+	float railWoodOffsetH_ = 0.3f;
+
+	public void create( Railling preRail, Vector2 pos, float poleHeight )
+	{
+		poleHeight_ = poleHeight;
+		poleWood_.transform.localScale = new Vector3( 1.0f, 1.0f, poleHeight );
+		transform.position = new Vector3( pos.x, pos.y, 0.0f );
+
+		if ( preRail != null ) {
+			float h0 = preRail.PoleHeight - railWoodOffsetH_;
+			float h1 = PoleHeight - railWoodOffsetH_;
+			var p0 = preRail.transform.position;
+			p0.z = -h0;
+			var p1 = new Vector3( pos.x, pos.y, -h1 );
+			Vector3 foward = p1 - p0;
+			float len = foward.magnitude;
+			railWood_.transform.forward = foward;
+			railWood_.transform.localPosition = new Vector3( 0.0f, 0.0f, -h1 );
+			railWood_.transform.localScale = new Vector3( 1.0f, 1.0f, len );
+		}
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
