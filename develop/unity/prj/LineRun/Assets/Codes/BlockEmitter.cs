66760648b20a8c5587e902f4a8a25035d3b66d34
marupeke_ikd <ikeyan@s2.dion.ne.jp>
2019-10-22 12:56:02 +0900

    ブロックを木のフェンスに差し替え

--- develop/unity/prj/LineRun/Assets/Codes/BlockEmitter.cs
+++ develop/unity/prj/LineRun/Assets/Codes/BlockEmitter.cs
@@ -8,6 +8,9 @@ public class BlockEmitter : MonoBehaviour
 	Block blockPrefab_;
 
 	[SerializeField]
+	Block blockPrefab2_;
+
+	[SerializeField]
 	Player player_;
 
 	[SerializeField]
@@ -15,6 +18,18 @@ public class BlockEmitter : MonoBehaviour
 
 	[SerializeField]
 	float averageInterval_ = 10.0f;
+
+	[SerializeField]
+	float minWidth_ = 1.0f;
+
+	[SerializeField]
+	float maxWidth_ = 1.5f;
+
+	[SerializeField]
+	float minHeight_ = 1.0f;
+
+	[SerializeField]
+	float maxHeight_ = 1.5f;
 
 
 	// ブロック出現平均間隔(sec)
@@ -30,11 +45,12 @@ public class BlockEmitter : MonoBehaviour
 
 	private void Awake()
 	{
-		AverageInterval = averageInterval_;
 	}
 
 	void Update()
     {
+		AverageInterval = averageInterval_;
+
 		if (averageInterval_ <= 0.0f) {
 			return;
 		}
@@ -42,9 +58,10 @@ public class BlockEmitter : MonoBehaviour
 		if (Random.value < p_) {
 			// Playerの後方にエミット
 			var pp = player_.transform.position;
-			var block = PrefabUtil.createInstance( blockPrefab_, transform );
-			block.emit( Random.Range( 3.0f, 10.0f ), 0.5f, Random.Range( 3.0f, 5.0f ), 0.0f );
+			var block = PrefabUtil.createInstance( Random.Range( 0, 2 ) == 0 ? blockPrefab_ : blockPrefab2_, transform );
+			block.emit( Random.Range( minWidth_, maxWidth_ ), 1.0f, Random.Range( minHeight_, maxHeight_ ), 0.0f );
 			block.transform.position = new Vector3( Randoms.Float.valueCenter() * field_.Width,	pp.y - 1.0f, 0.0f );
+			block.transform.rotation = Quaternion.Euler( 0.0f, 0.0f, Randoms.Float.valueCenter() * 45.0f );
 		}
 	}
 
---
+++ develop/unity/prj/LineRun/Assets/Codes/Explosion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 柵爆破
+//  吹き飛ばします
+
+public class Explosion : MonoBehaviour
+{
+	[SerializeField]
+	float radius_ = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
